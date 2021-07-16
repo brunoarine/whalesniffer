614638b0db8444723fbda9b943c139c567b44ec5
Bruno Arine <barine@runbox.com>
2021-07-16 15:47:33 -0300

    fix: Make Superpixels class Python 3-compatible

--- whalesniffer/body/superpixel.py
+++ whalesniffer/body/superpixel.py
@@ -95,13 +95,14 @@ class Superpixel():
         for filename in filenames_list:
             image = imread(filename)
             img_copy = skimage.transform.resize(image,(100,100,3))
-            img_copy = skimage.filters.gaussian(img_copy, sigma=0.5)
+            img_copy = skimage.filters.gaussian(img_copy, sigma=0.5, multichannel=True)
             img_norm = color.normalize_RGBratio(img_copy, method=self.method)
             if self.algorithm == 'slic':
                 seg = slic(img_norm, n_segments=200,
-                                                max_iter=100,
-                                                enforce_connectivity=True,
-                                                max_size_factor=10,)
+                           max_iter=100,
+                           enforce_connectivity=True,
+                           max_size_factor=10,
+                           start_label=0)
             elif self.algorithm == 'quickshift':
                 seg = quickshift(img_norm, kernel_size=3, sigma=0,
                                  convert2lab=True, max_dist=6, ratio=0.5)           
